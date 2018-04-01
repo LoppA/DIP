bc963afd4191f5594c7e6a1f953f67d588851ef8
LoppA <xLopacheco@gmail.com>
2018-04-01 11:58:50 -0300

    gamma

--- T2/t2.py
+++ T2/t2.py
@@ -3,10 +3,14 @@
 
 import numpy as np
 import imageio
-import matplotlib.pyplot as plt
 from math import sqrt
 
-def superres (L1, L2, L3, L4) :
+debug = 1
+
+if debug :
+    import matplotlib.pyplot as plt
+
+def superres(L1, L2, L3, L4):
     assert (L1.shape == L2.shape == L3.shape == L4.shape)
 
     dimL = L1.shape
@@ -31,43 +35,68 @@ def superres (L1, L2, L3, L4) :
 
     return H
 
+def hist(img):
+    return img
+
+def ajuste_gamma(img, gamma):
+    c = 255.0
+    img = (c * np.power(img.astype(np.float)/c, gamma))
+
+    return img.astype(np.uint8)
+
 imglow = str(input()).rstrip()
 imghigh = str(input()).rstrip()
 metodo = int(input())
-game = float(input())
+gamma = float(input())
 
 L1 = imageio.imread(imglow + '1.png')
 L2 = imageio.imread(imglow + '2.png')
 L3 = imageio.imread(imglow + '3.png')
 L4 = imageio.imread(imglow + '4.png')
 
-plt.subplot(221)
-plt.imshow(L1, cmap='gray')
-plt.axis('off')
-plt.subplot(222)
-plt.imshow(L2, cmap='gray')
-plt.axis('off')
-plt.subplot(223)
-plt.imshow(L3, cmap='gray')
-plt.axis('off')
-plt.subplot(224)
-plt.imshow(L4, cmap='gray')
-plt.axis('off')
-plt.show()
+if debug:
+    plt.subplot(221)
+    plt.imshow(L1, cmap='gray')
+    plt.axis('off')
+    plt.subplot(222)
+    plt.imshow(L2, cmap='gray')
+    plt.axis('off')
+    plt.subplot(223)
+    plt.imshow(L3, cmap='gray')
+    plt.axis('off')
+    plt.subplot(224)
+    plt.imshow(L4, cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+if metodo == 1:
+    L1 = hist(L1)
+    L2 = hist(L2)
+    L3 = hist(L3)
+    L4 = hist(L4)
+elif metodo == 3:
+    L1 = ajuste_gamma(L1, gamma)
+    L2 = ajuste_gamma(L2, gamma)
+    L3 = ajuste_gamma(L3, gamma)
+    L4 = ajuste_gamma(L4, gamma)
 
 H = superres(L1, L2, L3, L4)
+
+if metodo == 2:
+    H = hist(H)
 
 REF = imageio.imread(imghigh + '.png')
 
 assert (H.shape == REF.shape)
 
-plt.subplot(121)
-plt.imshow(H, cmap='gray')
-plt.axis('off')
-plt.subplot(122)
-plt.imshow(REF, cmap='gray')
-plt.axis('off')
-plt.show()
+if debug:
+    plt.subplot(121)
+    plt.imshow(H, cmap='gray')
+    plt.axis('off')
+    plt.subplot(122)
+    plt.imshow(REF, cmap='gray')
+    plt.axis('off')
+    plt.show()
 
 rmse = sqrt((1.0/(H.shape[0]*H.shape[1]))* np.sum(np.square(REF.astype(np.float) - H.astype(np.float))))
 
