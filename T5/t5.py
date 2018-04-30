e7045fa39cd25068f0d0ce3ec032e97e4aec9232
LoppA <xLopacheco@gmail.com>
2018-04-30 18:50:05 -0300

    go

--- T5/t5.py
+++ T5/t5.py
@@ -2,13 +2,48 @@
 
 import numpy as np
 import imageio
+from math import sqrt
+
+def filtra(Gk, M):
+    return Gk
+
+def conv(img, n, m):
+    return img
+
+def norm(img):
+    mn = np.min(img)
+    mx = np.max(img)
+
+    img = (img-mn)/(mx-mn)
+    img = (img * 255)
+
+    return img
+
+def insert(g0, gk):
+    return gk
+
+def gerchberg_papoulis(g, mask, T):
+    gant = g
+    M = np.fft.fft2(mask)
+
+    for k in range(1, T + 1):
+        Gat = np.fft.fft(gant)      # a done
+        Gat = filtra(Gat, M)        # b
+        gat = np.fft.ifft2(Gat)     # c done
+        gat = conv(gat, 7, 7)       # d
+        gat = norm(gat)             # e 
+        gat = insert(g, gat)        # f
+
+        gant = gat.astype(np.uint8)
+
+    return gant
 
 imgo = imageio.imread(str(input()).rstrip())    # imagem original
 imgi = imageio.imread(str(input()).rstrip())    # imagem deteriorada
 imgm = imageio.imread(str(input()).rstrip())    # mascara
 T = int(input())
 
-gant = imgi
-M = np.fft.fft2(imgm)
+res = gerchberg_papoulis(imgi, imgm, T)
 
-# for k in range(1, T + 1):
+rmse = sqrt(np.sum(imgo-res)**2)
+print("%.5f" % rmse)
