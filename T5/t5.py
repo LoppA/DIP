12fd603a2da515aca5daa6aebc30692f615e3c51
LoppA <xLopacheco@gmail.com>
2018-05-01 19:04:10 -0300

    af

--- T5/t5.py
+++ T5/t5.py
@@ -4,7 +4,7 @@ import numpy as np
 import imageio
 from math import sqrt
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Filtra Gk, zerando coeficientes das frequencias de Gk relativas a:
 # maiores ou iguais a 90% do maximo da magitude de M
@@ -15,15 +15,13 @@ def filtra(Gk, M):
 
 # Realiza convolucao de img com um filtro de media 'sz' x 'sz' no dominio da frequencia
 def conv(img, sz):
-#    filt = np.ones([sz, sz]) / (sz*sz)
-#    filt = np.fft.fft2(filt)
-
     filt = np.zeros(img.shape)
-    filt[0:7, 0:7] = np.ones([7, 7]) / (49.0)
+    filt[0:sz, 0:sz] = np.ones([sz, sz]) / (sz*sz)
     filt = np.fft.fft2(filt)
 
-    return filt*img
+    return np.multiply(filt,img)
 
+# normaliza a imagem em valores entra 0 e 255
 def norm(img):
     img = np.real(img)
     mn = np.min(img)
@@ -34,35 +32,38 @@ def norm(img):
 
     return img.astype(np.uint8)
 
+# insere os pixels conhecidos na estimativa
 def insert(gk, g0, mask):
     return (1-(mask/255))*g0 + (mask/255)*gk
 
+# funcao que aplica o algoritmo de Gerchberg-Papoulis em uma
+# imagem g, com mascara mask, iterando T vezes
 def gerchberg_papoulis(g, mask, T):
-    gat = g
-    M = np.fft.fft2(mask)
+    gk = g
+    M = np.fft.fft2(mask)   # transformada de Fourier da mascara M
 
-    plt.imshow(gat, cmap='gray')
-    plt.colorbar()
-    plt.show()
+#    plt.imshow(gk, cmap='gray')
+#    plt.colorbar()
+#    plt.show()
     
     for k in range(T):
-        Gat = np.fft.fft2(gat)      # a
+        Gk = np.fft.fft2(gk)      # transformada de Fourier da imagem gk
 
-        Gat = filtra(Gat, M)        # b
+        Gk = filtra(Gk, M)        # filtra Gk
 
-        gat = conv(gat, 7)          # d
+        gk = conv(gk, 7)          # convolucao de gk com filtro de media 7x7
 
-        gat = np.fft.ifft2(Gat)     # c
+        gk = np.fft.ifft2(Gk)     # transformada inversa
 
-        gat = norm(gat)             # e
+        gk = norm(gk)             # normaliza gk 
 
-        gat = insert(gat, g, mask)  # f 
+        gk = insert(gk, g, mask)  # insere os pixels conhecido
 
-        plt.imshow(gat, cmap='gray')
-        plt.colorbar()
-        plt.show()
+#        plt.imshow(gk, cmap='gray')
+#        plt.colorbar()
+#        plt.show()
 
-    return gat
+    return gk
 
 imgo = imageio.imread(str(input()).rstrip())    # imagem original
 imgi = imageio.imread(str(input()).rstrip())    # imagem deteriorada
