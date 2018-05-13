91c4a923ea941fc212c510e5813349c05014fab8
LoppA <xLopacheco@gmail.com>
2018-05-13 11:51:27 -0300

    filtro1++

--- T6/t6.py
+++ T6/t6.py
@@ -2,6 +2,19 @@
 
 import imageio
 import numpy as np
+import math
+
+try:
+    import matplotlib.pyplot as plt
+except Error:
+    pass
+
+def plot(img):
+    try:
+        plt.imshow(img, cmap='gray')
+        plt.show()
+    except Error:
+        pass
 
 def wrap(img, n):
     m = n//2
@@ -15,7 +28,7 @@ def wrap(img, n):
 
     return ret
 
-def filtro_adaptativo_reducao(img, n, alpha):
+def filtro_adaptativo_reducao(img, n, alpha, EPS = 0.001):
 #    img = np.zeros((3,3))
 #    for i in range(3):
 #        for j in range(3):
@@ -29,9 +42,30 @@ def filtro_adaptativo_reducao(img, n, alpha):
 #    print(img[3-1:3+1+1,3-1:3+1+1]) 
 
     ret = np.zeros(img.shape).astype(np.float)
-    img = wrap(img, n)
+    img = wrap(img, n).astype(np.float)
 
     var = alpha*alpha
+    m = n//2
+
+    for i in range(ret.shape[0]):
+        for j in range(ret.shape[1]):
+            ii = i + m
+            jj = j + m
+
+            filtro = img[ii-m:ii+m+1, jj-m:jj+m+1]
+
+            media = np.mean(filtro)
+            var_local = np.var(filtro)
+
+            assert(ii-m >= 0)
+            assert(jj-m >= 0)
+            assert(ii + m + 1 <= img.shape[0])
+            assert(jj + m + 1 <= img.shape[1])
+
+            if(var_local < EPS):
+                ret[i,j] = img[ii,jj]
+            else:
+                ret[i,j] = img[ii,jj] - (var*(img[ii,jj] - media))/var_local
 
     return ret.astype(np.uint8)
 
@@ -40,6 +74,8 @@ Icomp = imageio.imread(str(input()).rstrip())
 Inoisy = imageio.imread(str(input()).rstrip())
 op = int(input())
 N = int(input())
+
+plot(Inoisy)
 
 if op == 1:
     alpha = float(input())
@@ -51,5 +87,8 @@ else:
 
 assert(Iout.shape == Icomp.shape)
 
-R, S, _  = Iout.shape
+plot(Iout)
+
+R, S  = Iout.shape
 rmse = np.sqrt(np.sum(np.square(Icomp - Iout)) / (R * S))
+print("%.5f" % rmse)
