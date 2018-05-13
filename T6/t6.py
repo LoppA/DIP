573f62d13028b2991605e7a0a0a10cc6b0c33fa8
LoppA <xLopacheco@gmail.com>
2018-05-13 12:53:14 -0300

    bug fixed

--- T6/t6.py
+++ T6/t6.py
@@ -44,35 +44,45 @@ def filtro_adaptativo_reducao(img, n, alpha, EPS = 0.001):
 
     return ret.astype(np.uint8)
 
-def filtro_adaptativo_reducao(img, n, alpha, EPS = 0.001):
+def filtro_adaptativo_mediana(img, n, M):
     ret = np.zeros(img.shape).astype(np.float)
-    img = wrap(img, n).astype(np.float)
-
-    var = alpha*alpha
-    m = n//2
+    img = wrap(img, M).astype(np.float)
 
     for i in range(ret.shape[0]):
         for j in range(ret.shape[1]):
-            ii = i + m
-            jj = j + m
+            ii = i + M//2
+            jj = j + M//2
 
-            filtro = img[ii-m:ii+m+1, jj-m:jj+m+1]
+            while(n <= M):
+                m = n//2
 
-            media = np.mean(filtro)
-            var_local = np.var(filtro)
+                if(n%2 == 1):
+                    filtro = img[ii-m:ii+m+1, jj-m:jj+m+1]
+                else:
+                    filtro = img[ii-m+1:ii+m+1, jj-m+1:jj+m+1]
 
-            assert(ii-m >= 0)
-            assert(jj-m >= 0)
-            assert(ii + m + 1 <= img.shape[0])
-            assert(jj + m + 1 <= img.shape[1])
+                zmed = np.median(filtro)
+                zmin = np.amin(filtro)
+                zmax = np.amax(filtro)
 
-            if(var_local < EPS):
-                ret[i,j] = img[ii,jj]
-            else:
-                ret[i,j] = img[ii,jj] - (var*(img[ii,jj] - media))/var_local
+                a1 = zmed - zmin
+                a2 = zmed - zmax
+
+                if(a1 > 0 and a2 < 0):
+                    b1 = img[ii, jj] - zmin
+                    b2 = zmed - zmax
+                    if(b1 > 0 and b2 < 0):
+                        ret[i,j] = img[ii,jj]
+                    else:
+                        ret[i,j] = zmed
+                    break
+                else:
+                    n+=1
+                    if(n > M):
+                        ret[i,j] = zmed
+    
 
     return ret.astype(np.uint8)
-
 
 def filtro_media_contra_harmonica(img, n, Q):
     ret = np.zeros(img.shape).astype(np.float)
@@ -128,6 +138,7 @@ plt.show()
 plt.imshow(Iout, cmap='gray')
 plt.show()
 '''
+
 R, S  = Iout.shape
 rmse = np.sqrt(np.sum(np.square(Icomp - Iout)) / (R * S))
 print("%.5f" % rmse)
